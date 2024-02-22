48963343baaf2e2929a73b701692ae4b4c58bea5
gen <kyliki@yandex.ru>
2024-02-22 23:38:30 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,59 @@
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  settings: {
+    "import/resolve": {
+      "eslint-import-resolver-custom-alias": {
+        "alias": {
+          "@": "./src",
+          "@components": "./src/components"
+        }
+    }
+  },
+
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'airbnb',
+    'prettier',
+  ],
+
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  plugins: ['vue'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: false,
+      },
+    ],
+  },
+}}
+
